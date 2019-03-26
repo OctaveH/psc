a669c55214d2ebe638c235bbc21c62af2935d451
Octave Hazard <octave.hazard@polytechnique.edu>
2019-03-26 15:32:08 +0100

    ballon de rugby

--- src/human_modelling.cpp
+++ src/human_modelling.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <ode/ode.h>
+#include <drawstuff/drawstuff.h>
 
 #include "../include/util.h"
+
+static dWorldID world;
+static dSpaceID space;
+
+const dReal density = 1.0;
+const dReal length = 0.8;
+const dReal radius = 0.2;
+
+dBodyID cap;
+dGeomID geom;
+static dGeomID  ground;
+static dJointGroupID contactgroup;
+//static int flag = 0;
+dsFunctions fn;
+
+//const dReal   radius = 0.2;
+//const dReal   mass   = 1.0;
+
+typedef struct {
+  dBodyID body;
+  dGeomID geom;
+} MyObject;
+MyObject ball;
 
 void test1() // test partie 1 opération sur les vecteurs
 {
@@ -46,8 +71,117 @@ void test1() // test partie 1 opération sur les vecteurs
     std::cout << "sign(n): " << s << ",  norm(u): " << m << ", dot3(u,v): " << t << ", angle3(u,v): " << z << std::endl;
 }
 
-int main()
+//start function
+void start()
 {
-    test1();
+    //Set a camera
+    static float xyz[3] = {0.0, -5.0, 1.0};
+    static float hpr[3] = {90.0, 0.0, 0.0};
+    dsSetViewpoint(xyz, hpr);
+}
+
+// this is called by dSpaceCollide when two objects in space are
+// potentially colliding.
+static void nearCallback (void *data, dGeomID o1, dGeomID o2)
+{
+   dBodyID b1 = dGeomGetBody(o1);
+   dBodyID b2 = dGeomGetBody(o2);
+   dContact contact;
+   contact.surface.mode = dContactBounce | dContactSoftCFM;
+   // friction parameter
+   contact.surface.mu = 500;
+   // bounce is the amount of "bouncyness".
+   contact.surface.bounce = 0.2;
+   // bounce_vel is the minimum incoming velocity to cause a bounce
+   // contact.surface.bounce_vel = 0.001;
+   // constraint force mixing parameter
+   // contact.surface.soft_cfm = 0.001;
+   if (int numc = dCollide (o1,o2,1,&contact.geom,sizeof(dContact))) {
+       dJointID c = dJointCreateContact (world,contactgroup,&contact);
+       dJointAttach (c,b1,b2);
+   }
+}
+
+static void simLoop (int pause)
+{
+    const dReal *pos, *R; // position, rotation matrix
+
+    dSpaceCollide(space, 0, &nearCallback);
+
+    dWorldStep(world, 0.05); // Step a simulation world, time step is 0.05
+
+    dJointGroupEmpty(contactgroup);
+
+    dsSetColor(0.5, 0.2, 0.2);
+    pos = dBodyGetPosition(cap);
+    R = dBodyGetRotation(cap);
+    dsDrawCapsuleD(pos, R, length, radius);
+}
+
+//create a capsule body and corresponding geom.
+void createCapsule(dBodyID* body, dGeomID* geom, dWorldID world, dSpaceID space, dReal density, dReal length, dReal radius)
+{
+    //create capsule body (aligned along the z-axis so that it matches the
+	//GeomCCylinder created below, which is aligned along the z-axis by
+	//default)
+	*body = dBodyCreate(world);
+	dMass M;
+	dMassSetZero(&M);
+	dMassSetCapsule(&M, density, 3, radius, length);
+	dBodySetMass(*body, &M);
+
+	// Je sais pas comment écrire les trois lignes, "set parameters for drawing the body
+
+
+	// create a capsule geom for collision detection
+	*geom = dCreateCapsule(space, radius, length);
+	dGeomSetBody(*geom, *body);
+}
+
+int main(int argc, char **argv)
+{
+    //dReal x0 = 0.0, y0 = 0.0, z0 = 2.0;
+    //dMass m1;
+
+    //for drawstuff
+    dsFunctions fn;
+    fn.version = DS_VERSION; //the version of drawstuff
+    fn.start = &start;
+    fn.step = &simLoop;
+    fn.command = NULL; //no command function for keyboard
+    fn.stop = NULL; //no stop function
+    fn.path_to_textures = "../../drawstuff/textures";
+
+    dInitODE();
+    world = dWorldCreate();
+    space = dHashSpaceCreate(0);
+    contactgroup = dJointGroupCreate(0);
+    dWorldSetGravity(world,0,0,-0.005);
+
+    // set the initial simulation loo parameters
+    const int fps = 60;
+    const dReal dt = 1.0 / fps;
+    const int stepsPerFrame = 2;
+    const int SloMo = 1;
+    bool Paused = false;
+    int numiter = 0;
+
+    //create a capsule
+    createCapsule(&cap, &geom, world, space, density, length, radius);
+    dBodySetPosition(cap, 0.0, 0.0, 2.0);
+    dMatrix3 R;
+    dRFromAxisAndAngle(R, 1.0, 1.0, 0.0, 0.01);
+    dBodySetRotation(cap, R);
+    ground = dCreatePlane(space, 0, 0, 1, 0);
+
+    //Simulation loop
+    // argc, argv are argument of main function.
+    //Window size is 352 x 288 pixels
+    //fn is a structure of drawstuff
+    dsSimulationLoop(argc, argv, 352*3, 288*3, &fn);
+
+    dWorldDestroy(world); // Destroy the world
+    dCloseODE();          // Close ODE
+
     return 0;
 }
